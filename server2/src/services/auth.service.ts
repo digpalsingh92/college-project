091ccd7742d1dad734a32b83f1ed3e22872faafd
Digpal Singh <digpalsingh18901@gmail.com>
2026-04-03 23:57:12 +0530

    changes done for auth

--- server2/src/services/auth.service.ts
+++ server2/src/services/auth.service.ts
@@ -1,4 +1,7 @@
 import prisma from "../lib/prisma.js"
+import type { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { AppError } from '../utils/app-error.js';
 
 export const login = async (req: Request, res: Response): Promise<void> => {
     const user = await prisma.users.findUnique({
@@ -6,3 +9,14 @@ export const login = async (req: Request, res: Response): Promise<void> => {
             email: req.body.email
         }
     });
+    if (!user) {
+        res.status(404).json({ error: "User not found" });
+        return;
+    }
+
+    const passwordMatches = await bcrypt.compare(req.body.password, user.passwordHash);
+  if (!passwordMatches) {
+    throw new AppError('Invalid email or password', 401);
+  }
+
+}
